890d18db48582ddb34ed0880104c76f53dc04754
Juan <juandavid2002111@gmail.com>
2024-07-21 15:31:12 -0400

    prueba 2

--- src/main/Controlador/App.java
+++ src/main/Controlador/App.java
@@ -6,8 +6,6 @@ public class App {
     public static void main(String[] args) {
         B B=new B();
         A A=new A();
-
-
         System.out.println(B.a);
         System.out.println(A.a);
     }
