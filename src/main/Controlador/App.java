3d58b0a8a092945b20eb405fa8c9b57ee627551e
Juan <juandavid2002111@gmail.com>
2024-07-21 10:42:44 -0400

    Carpetas pruebas

---
+++ src/main/Controlador/App.java
@@ -0,0 +1,15 @@
+package Controlador;
+import Modelo.A;
+import Modelo.B;
+
+public class App {
+    public static void main(String[] args) {
+        B B=new B();
+        A A=new A();
+
+
+        System.out.println(B.a);
+        System.out.println(A.a);
+    }
+
+}
---
+++ src/main/Modelo/B.java
@@ -0,0 +1,7 @@
+package Modelo;
+
+public class B {
+    public
+    String a="hola";
+
+}
